9edbbb9fb3d98622fcb93ab378517be857b216f8
MohammedYaseenRon <yaseenron3@gmail.com>
2024-12-05 18:38:23 +0530

    added shadcn and made simple home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,19 @@
+import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello world!
+    <div className="bg-white w-full h-screen">
+      <Navbar />
+      <div className="flex justify-center items-center p-16 md:p-32">
+        <div className="max-w-2xl space-y-6 text-center">
+          <Button className="bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-xl px-6 py-3">Be ahead of everyone</Button>
+          <h1 className="text-4xl md:text-6xl font-bold text-gray-800">Get Hired with Your Work</h1>
+          <p className="text-xl md:text-2xl font-semibold text-red-600">Join Talents Tracks and turn your projects into opportunities.</p>
+          <Button className="bg-black hover:bg-gray-800 text-white font-medium rounded-full px-8 py-4">Get started</Button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React,{useState} from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image';
+import { User } from 'lucide-react';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div className='bg-red-400 shadow-lg w-full h-16 rounded-lg'>
+        <div className='max-w-screen-xl mx-auto px-4 flex items-center justify-between h-full'>
+            <div className='flex items-center space-x-2'>
+                {/* <Link href="/"><Image 
+                src="/"
+                alt='Talent Tracks'
+                width={100}
+                height={100}
+                className='h-full' /></Link> */}
+                <span className='text-xl text-gray-800 font-bold'>Talents Tracks</span>
+            </div>
+            <div className='flex items-center ml-auto space-x-3'>
+                <Button className='bg-white text-gray-700 hover:bg-gray-100 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0'>
+                    <User size={10} />
+                </Button>
+                <Button className='text-base'>Get Started</Button>  
+            </div> 
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
