728f1936185fa40aa7b3c064f27b0fdcbcfc25b7
MohammedYaseenRon <yaseenron3@gmail.com>
2025-03-08 13:09:10 +0530

    completed

--- server/src/index.ts
+++ server/src/index.ts
@@ -41,7 +41,7 @@ wss.on("connection", (ws: WebSocket) => {
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://talenttrack-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
