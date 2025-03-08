15dd533ae0b3d2fcad5a157259fef711562d8efc
MohammedYaseenRon <yaseenron3@gmail.com>
2025-03-08 13:18:43 +0530

    cors issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -41,7 +41,7 @@ wss.on("connection", (ws: WebSocket) => {
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-    origin: ["http://localhost:3000","https://talenttrack-frontend.vercel.app"],
+    origin: ["http://localhost:3000","https://talent-track-liard.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
